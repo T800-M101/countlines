f080819703dd5381df89fb359021fb78b51252c0
memo <tuahil@gmail.com>
2023-06-07 19:13:50 -0600

    add logic to countlines.sh working with command substitution

--- countlines.sh
+++ countlines.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 # This bash script counts the number of lines in a directory passing as argument the owner of the directory or the month when the files where created.
+
+echo -n "Please enter a filename: "
+
+read filename
+
+nlines=$(wc -l < $filename)
+
+echo "There are $nlines lines in $filename"
