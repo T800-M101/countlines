52b6a8ea870b04abdb01807e72ba535d0555ad39
memo <tuahil@gmail.com>
2023-06-10 08:16:05 -0600

    added help function to countlines.sh script

--- countlines.sh
+++ countlines.sh
@@ -3,6 +3,27 @@
 # This bash script counts the number of lines in a directory passing as argument the owner of the directory or the month when the files where created.
 
 
+# Function to provide help on how to use the script
+function help(){
+     echo -e  "\e[1;31m***** THIS SMALL HELP WILL SHOW YOU HOW TO USE THE countlines.sh BASH SCRIPT *****\e"
+     echo -e "\e[0;37m'countlines.sh' bash script will count the number of lines of a file that belongs to an owner or the number of lines of a file created at any specific month.\e"
+     echo
+     echo -e "\e[1;32mTo count the number of lines of files owned by a specific onwer, use  the owner option:\e"
+     echo -e "\e[0;37m./countlines.sh -o"
+     echo -e "./countlines.sh --owner\e"
+     echo
+     echo -e "\e[1;32mTo count the number of lines of files created at an specific month, use the month option\e"
+     echo -e "\e[0;37m./countlines.sh -m"
+     echo "./countlines.sh -month\e"
+     echo
+     echo "-o or --owner is the owner of the files and is optional."
+     echo "-m or --month is the month in which the files were created and is optioan"
+     echo
+     echo "*** If you have more questions you can google it :)*** "
+}
+
+
+
 # ***** SCRIPT INIT ******
     # Validating if there are options
     if [[ $# -gt 0 ]]
