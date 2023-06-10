f1c51007e6865ec3fdc4f2622700a62f715b66e5
memo <tuahil@gmail.com>
2023-06-10 08:14:03 -0600

    added options menu to countlines.sh script

--- countlines.sh
+++ countlines.sh
@@ -3,6 +3,27 @@
 # This bash script counts the number of lines in a directory passing as argument the owner of the directory or the month when the files where created.
 
 
-nlines=$(wc -l < $1)
+# ***** SCRIPT INIT ******
+    # Validating if there are options
+    if [[ $# -gt 0 ]]
+        then
+            key=$1
 
-echo "There are $nlines lines in $1"
+            case $key in 
+                "-o" | "--owner")
+                     owner=$2
+                     get_lines_by_owner $owner
+                     ;;
+
+                "-m" | "--month")
+                    month=$2
+                    get_lines_by_month $month
+                    ;;
+
+                *)
+                help
+                ;;
+             esac
+    else
+        echo "Please provide an option: ./countlines.sh -o or ./countlines.sh -m"
+    fi
